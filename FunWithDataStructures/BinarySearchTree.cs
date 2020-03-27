64f0fc583d4f0c96f46c8eef69e1acfa44f7643c
Katherine Hambley <superdesigngirl@mac.com>
2020-03-27 09:31:59 -0500

    Added Initial project files

---
+++ FunWithDataStructures/BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithDataStructures
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> _root;
+
+        //return a TreeNode by searching for a value
+        public TreeNode<T> Get(T value)
+        {
+            return _root?.Get(value); //? = if _root is not null
+        }
+
+        public T Min()
+        {
+            if (_root == null)
+            {
+                throw new InvalidOperationException(message: "Tree is empty");
+            }
+            return _root.Min();
+        }
+
+        public T Max()
+        {
+            if (_root == null)
+            {
+                throw new InvalidOperationException(message: "Tree is empty");
+            }
+            return _root.Max();
+        }
+
+        public void Insert(T value)
+        {
+            if (_root == null) //tree is empty so initialize it
+            {
+                _root = new TreeNode<T>(value);
+            }
+            else
+            {
+                _root.Insert(value);
+            }
+        }
+
+        public IEnumerable<T> TraverseInOrder()
+        {
+            if(_root != null)
+            {
+                return _root.TraverseInOrder();
+            }
+
+            return Enumerable.Empty<T>(); //best practice, instead of returning null
+        }
+    }
+}
---
+++ FunWithDataStructures/TreeNode.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithDataStructures
+{
+    public class TreeNode<T> where T : IComparable<T>
+    {
+        public T Value { get; private set; }
+        public TreeNode<T> Left { get; private set; }
+        public TreeNode<T> Right { get; private set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+
+        public void Insert(T newValue)
+        {
+            int compare = newValue.CompareTo(Value);
+            if(compare == 0)
+            {
+                return;
+            }
+            if (compare < 0)
+            {
+                if(Left == null)
+                {
+                    Left = new TreeNode<T>(newValue);
+                }
+                else
+                {
+                    Left.Insert(newValue);
+                }
+            }
+            else
+            {
+                if(Right == null)
+                {
+                    Right = new TreeNode<T>(newValue);
+                }
+                else
+                {
+                    Right.Insert(newValue);
+                }
+            }
+        }
+
+        public TreeNode<T> Get(T value)
+        {
+            int compare = value.CompareTo(Value);
+
+            if (compare == 0) //return current node
+            {
+                return this;
+            }
+
+            if(compare < 0)
+            {
+                if (Left != null) //Recursively going down the tree
+                {
+                    return Left.Get(value);
+                }
+                else
+                {
+                    if(Right != null)
+                    {
+                        return Right.Get(value);
+                    }
+                    
+                }
+            }
+            return null; //if value not found
+        }
+
+        public IEnumerable<T> TraverseInOrder()
+        {
+            var list = new List<T>();
+            InnerTraverse(list);
+            return list;
+        }
+
+        private void InnerTraverse(List<T> list)
+        {
+            if(Left != null)
+            {
+                Left.InnerTraverse(list);
+            }
+            list.Add(Value);
+
+            if(Right != null)
+            {
+                Right.InnerTraverse(list);
+            } 
+        }
+        public T Min() //go down the leftmost node and get value
+        {
+            if(Left != null)
+            {
+                return Left.Min();
+            }
+            return Value;
+        }
+        public T Max() //go down the rightmost node and get value
+        {
+            if (Right != null)
+            {
+                return Right.Max();
+            }
+            return Value;
+        }
+    }
+}
